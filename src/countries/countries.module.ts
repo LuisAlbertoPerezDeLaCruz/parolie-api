593c4858c2e56d1c7ce7b0aee741f3e0617413d3
lap <luisalbertoperez@gmail.com>
2021-11-24 19:20:07 -0400

    ajustes

--- src/countries/countries.module.ts
+++ src/countries/countries.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { CountriesService } from './countries.service';
 import { CountriesController } from './countries.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Country, CountrySchema } from './schemas/country.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Country.name,
+        useFactory: () => {
+          const schema = CountrySchema;
+          schema.plugin(require('mongoose-autopopulate'));
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [CountriesController],
-  providers: [CountriesService]
+  providers: [CountriesService],
 })
 export class CountriesModule {}
